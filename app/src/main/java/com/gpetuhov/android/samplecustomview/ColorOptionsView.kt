e8c4f4c304c94608b8bf2ed8ae632ebc3c8e2c31
Gennady Petukhov <gpetukhov@phoenixit.ru>
2019-03-21 17:48:08 +0300

    Add custom view

---
+++ app/src/main/java/com/gpetuhov/android/samplecustomview/ColorOptionsView.kt
@@ -0,0 +1,59 @@
+package com.gpetuhov.android.samplecustomview
+
+import android.content.Context
+import android.graphics.Color
+import android.util.AttributeSet
+import android.view.View
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import android.view.LayoutInflater
+import android.view.Gravity
+
+
+class ColorOptionsView(context: Context, attrs: AttributeSet?) : LinearLayout(context, attrs) {
+
+    private var mValue: View? = null
+    private var mImage: ImageView? = null
+
+    constructor(context: Context): this(context, null)
+
+    init {
+        initView(context, attrs)
+    }
+
+    private fun initView(context: Context, attrs: AttributeSet?) {
+        val attributes = context.obtainStyledAttributes(
+            attrs,
+            R.styleable.ColorOptionsView, 0, 0
+        )
+        val titleText = attributes?.getString(R.styleable.ColorOptionsView_titleText)
+        val valueColor = attributes?.getColor(
+            R.styleable.ColorOptionsView_valueColor,
+            Color.BLUE
+        ) ?: Color.BLUE
+        attributes?.recycle()
+
+        orientation = HORIZONTAL
+        gravity = Gravity.CENTER_VERTICAL
+
+        val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        inflater.inflate(R.layout.view_color_options, this, true)
+
+        val title = getChildAt(0) as TextView
+        title.text = titleText
+
+        mValue = getChildAt(1)
+        mValue?.setBackgroundColor(valueColor)
+
+        mImage = getChildAt(2) as ImageView
+    }
+
+    fun setValueColor(color: Int) {
+        mValue?.setBackgroundColor(color)
+    }
+
+    fun setImageVisible(visible: Boolean) {
+        mImage?.visibility = if (visible) View.VISIBLE else View.GONE
+    }
+}
--- app/src/main/java/com/gpetuhov/android/samplecustomview/MainActivity.kt
+++ app/src/main/java/com/gpetuhov/android/samplecustomview/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.gpetuhov.android.samplecustomview
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var colorChange = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        view1.setOnClickListener { view1.setValueColor(getNextColor(Color.RED)) }
+        view2.setOnClickListener { view2.setValueColor(getNextColor(Color.BLUE)) }
+    }
+
+    private fun getNextColor(color: Int): Int {
+        val result = color + colorChange
+        colorChange += 100
+        return result
     }
 }
